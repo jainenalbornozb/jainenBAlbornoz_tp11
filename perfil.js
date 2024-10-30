0dc64ff9fcc9f1be0d6a6478d82851c4e474af85
jainenb <jainenbanqueroalbornoz@gmail.com>
2024-10-29 23:25:00 -0300

    trabajo practico de condicionales

---
+++ perfil.js
@@ -0,0 +1,16 @@
+//asignación de perfil
+let perfil = "asistente";
+
+//verificación de permisos en función del perfil
+
+if (perfil === "") {
+    console.log("Debe especificar el perfil del usuario");
+} else if (perfil.toLowerCase() === "administrador") {
+    console.log("Usted tiene todos los privilegios de uso del sistema");
+} else if (perfil.toLowerCase() === "asistente") {
+    console.log("Usted sólo tiene permisos de registrar, modificar y consultar datos");
+} else if (perfil.toLowerCase() === "invitado") {
+    console.log("Usted sólo tiene permisos de consultar datos");
+} else {
+    console.log("Debe especificar un perfil válido");
+}
